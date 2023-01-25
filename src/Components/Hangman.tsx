b5c5fbac1cfdbdb80e6ab3798ba1ac43521e80d9
Sweta Sinha <sweta@Swetas-MacBook-Pro.local>
2023-01-25 20:09:01 +0000

    Added auto focus

--- src/Components/Hangman.tsx
+++ src/Components/Hangman.tsx
@@ -1,18 +1,23 @@
-import React,{useState, useEffect} from 'react'
+import React,{useState, useRef, useEffect} from 'react'
 
 export default function Hangman() {
     const word = "Bedsheet".toUpperCase();
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
     const [guessed, setGuessed] = useState<string | any>([])
     const [lives, setLives] = useState(10)
     const maskedWord = word.split("").map(item => guessed.includes(item) ? item : "__").join(" ")
+
+    useEffect(()=>{
+        if(inputRef.current) inputRef.current.focus();
+    },[guessed])
   return (
     <div>
       <h2>Welcome to Hangman</h2>
       <h3>Total Lives Left: {lives<=0 ? 0: lives}</h3>
       <h3>{maskedWord}</h3>
       <label>Enter your letter: </label>
-      <input type="text" onChange={(e)=> {
+      <input ref={inputRef} type="text" onChange={(e)=> {
         if(word.includes(e.target.value.toUpperCase())){
             setGuessed([...guessed, e.target.value.toUpperCase()])
         }else{setLives(lives-1)}
